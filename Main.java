391f3365f3966cc203c21321af59518f41b9bd39
evankim1002-boop <evankim1002@gmail.com>
2026-05-29 21:38:55 -0700

    test mode

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main {
         Game game1 = new Game();
         while(game1.checkWin(game1.getBoard()) == 0 && game1.getGameOver() == 0){
             game1.playerChoice();
+            game1.printDebug();
         }
+        game1.printDebug();
         if (game1.checkWin(game1.getBoard()) == 1){
             System.out.println("You Win!");
         }
