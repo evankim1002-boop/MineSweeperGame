fcf40974ee7168c512b9c161620e72f68b37b0f4
evankim1002-boop <evankim1002@gmail.com>
2026-05-29 21:45:54 -0700

    Made Game over more visible

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ public class Main {
         Game game1 = new Game();
         while(game1.checkWin(game1.getBoard()) == 0 && game1.getGameOver() == 0){
             game1.playerChoice();
+            System.out.println();
             game1.printDebug();
         }
         game1.printDebug();
         if (game1.checkWin(game1.getBoard()) == 1){
+            System.out.println();
             System.out.println("You Win!");
+            System.out.println();
         }
     }
 }
